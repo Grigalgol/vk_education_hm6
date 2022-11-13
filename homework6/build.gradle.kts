336bad3b7c38d89171a17a009a5314277af10e0e
Gregory <grisha101202@yanex.ru>
2022-11-13 10:36:58 +0300

    init project

---
+++ homework6/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.22")
+    implementation("org.flywaydb:flyway-core:9.7.0")
+    implementation("org.postgresql:postgresql:42.5.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
